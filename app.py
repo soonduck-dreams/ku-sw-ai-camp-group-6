5fc71521d19307ba1da647d32851e3ffcb0b336d
Junseok Lee <6seok0508@gmail.com>
2024-08-05 17:06:36 +0900

    준석 수정본

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from logics.example_logics import get_greeting, summarize_text
 
-st.title('LLM Web Application')
+st.title('LLM Web Application game')
 
 if st.button('Greet'):
     greeting = get_greeting()
@@ -13,3 +13,4 @@ text_to_summarize = st.text_area("Enter text to summarize")
 if st.button('Summarize'):
     summary = summarize_text(text_to_summarize)
     st.write(summary)
+
