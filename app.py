69b4328045977e25f49bd85e624a528817473e02
soonduck <nederileni99heits@gmail.com>
2024-08-06 16:12:24 +0900

    feat: 임시 웹 UI 제작

--- app.py
+++ app.py
@@ -2,15 +2,22 @@
 
 import streamlit as st
 from logics.example_logics import get_greeting, summarize_text
+from prompts.initial_prompts import initial_prompt
 
-st.title('LLM Web Application 준희')
+st.title('ADY: AI Docent For You')
 
-if st.button('Greet'):
-    greeting = get_greeting()
-    st.write(greeting)
+if "messages" not in st.session_state:
+  st.session_state.messages = initial_prompt
 
-text_to_summarize = st.text_area("Enter text to summarize")
-if st.button('Summarize'):
-    summary = summarize_text(text_to_summarize)
-    st.write(summary)
+for index, message in enumerate(st.session_state.messages):
+  if message["role"] != "system":
+      with st.chat_message(message["role"]):
+          st.markdown(message["content"])
 
+user_prompt = st.chat_input()
+if user_prompt:
+  st.session_state.messages.append({'role': 'user', 'content': user_prompt})
+  with st.chat_message('user'):
+    st.markdown(user_prompt)
+  
+  # TODO: 모델한테 메시지 보내고 받아서 표시하는 거 추가하기
---
+++ prompts/initial_prompts.py
@@ -0,0 +1,3 @@
+initial_prompt = [
+  {'role': 'system', 'content': '너는 사용자의 미술 작품 감상을 돕는 AI 도슨트이다. 너의 이름은 ADY(아디)이다.'}
+]
