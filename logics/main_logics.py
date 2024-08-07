ef1458afa80781e0282ed833c3d4f78e47284266
jangjh1907 <jangjh1907@gmail.com>
2024-08-07 15:20:07 +0900

    필요없는 코드 정리, query를 기반으로 data(string)정리해 return하는 함수 추가

--- logics/main_logics.py
+++ logics/main_logics.py
@@ -13,6 +13,7 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 
 client = OpenAI(api_key=openai_api_key)
 
+
 def get_embedding(input):
     #input을 임베딩한 결과를 return하는 함수
     response = client.embeddings.create(
@@ -22,72 +23,54 @@ def get_embedding(input):
     return response.data
 
 
-def extract_keyword(text):
-    #query에서 keyword만을 추출하도록 LLM에게 시키는 함수
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"user": "user", "content": text},
-            {"role": "system", "content": extract_keyword_prompt}
-        ]
-    )
-    return response
-
-
-def decide_dbart_only(query):
-    #query가 db_art만을 활용한 답안을 내놓아야 할 질문인지 아닌지를 T/F (bool 타입)으로 대답한다.
-    #대답을 생성하는 과정 자체에는 db_art와 db_etc를 모두 활용할 수 있으나, 대답에 들어갈 text는 db_art에서만 뽑아와야 할 것.
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=if_dbart_only_messages + [
-            {"role": "user", "content": query},
-        ],
-    )
-    if response == "True":
-        if_dbart_only = True
-    else:
-        if_dbart_only = False
-    return if_dbart_only
+def artdata_to_string(data):
+    ret_str = ""
+    for each_key in data.keys():
+        ret_str += "[" + each_key + "]: " + data[each_key]
+    return ret_str
 
 
 def get_data_from_db(query, db_art, db_etc):
-    #query: user의 질문
-    #db_art: art관련 db
-    #db_etc: 기타 내용 관련 db
+    """query와 관련한 DB의 data를 검색해서 string 형태로 도출
+    Args:
+        messages: 지금껏 주고받은 message 기록 ex)st.session_state.messages
+        db_art: 예술품 DB, db_etc: 기타 DB
+    """
 
-    #query_keyword = extract_keyword(query)  
-    #query_keyword: query의 keyword만을 추출
     query_embed = get_embedding(query)[0].embedding
     #query_embed: query_keyword를 바탕으로 embedding을 추출 (float list)
 
-    if_dbart_only = decide_dbart_only(query)
-    #if_dbart_only: query가 특정 작품에 대한 설명만을 대답으로 하는 질문이라면 True, 아니라면 False (bool type)
+    art_idx = faiss.IndexFlatL2(len(query_embed))
+    etc_idx = faiss.IndexFlatL2(len(query_embed))
 
-    
+    db_art_emb = db_art[1]
+    db_etc_emb = db_etc[1]
 
+    art_idx.add(np.array(db_art_emb))
+    etc_idx.add(np.array(db_etc_emb))
 
-    # 현재 여기까지 진행중
+    art_dists, art_idxs = art_idx.search(np.array([query_embed]), 3)
+    etc_dists, etc_idxs = etc_idx.search(np.array([query_embed]), 7)
 
-    index = faiss.IndexFlatL2(len(query_embed))
+    data_string = ""
+    data_string += "예술품 관련 data 목록: ["
+    for i in art_idxs[0]:
+        data_string += "예술품 data" + str(i + 1) + ": " + artdata_to_string(db_art[0][i]) + "//"
+    data_string += "]"
+    data_string += "기타 다양한 data 목록: ["
+    for i in etc_idxs[0]:
+        data_string += "기타 data" + str(i + 1) + ": " + db_etc[0][i] + "//"
+    data_string += "]"
 
-    db_embed = db[1]
-    index.add(np.array(db_embedding))
+    print(data_string)      #TEST
 
-    k = 5
-    distances, indices = index.search(np.array([query_keyword]), k)
-
-    input_string = ""
-    for i in indices[i]:
-        input_string += "data" + str(i + 1) + ": " + db[0][i] + "//"
-
-
+    return data_string
 
 
 
 
 def ask(messages, db_art=None, db_etc=None):
-    """사용자의 질문에 대해 답하는 기본 함수 만드는
-
+    """사용자의 질문에 대해 답하는 기본 함수
     Args:
         messages: 지금껏 주고받은 message 기록 ex)st.session_state.messages
         db_art: 예술품 DB, db_etc: 기타 DB
